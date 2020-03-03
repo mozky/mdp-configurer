474c7ce237da2ddd794e7c35ed181ddd52ad8909
mozky <moz@mozky.dev>
2020-03-02 23:05:33 -0600

    Add log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core")
 
 try {
+  console.log('Getting configuration for project', process.env)
   console.log(process.env)
 } catch (err) {
   core.setFailed(err.message)
