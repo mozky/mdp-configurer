90605d895b1f3fd5c836992c3e63710a61b0f1fb
mozky <moz@mozky.dev>
2020-03-02 23:12:56 -0600

    Add github actions package

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const core = require("@actions/core")
+const github = require("@actions/github")
 
 try {
   console.log('Getting configuration for project', process.env)
-  console.log(process.env)
+  console.log('Project Id input', core.getInput('project-id'))
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
 } catch (err) {
   core.setFailed(err.message)
 }
