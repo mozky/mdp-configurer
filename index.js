87ba137f535bc12bea169845114d583117b8fdb5
mozky <moz@mozky.dev>
2020-03-03 20:22:43 -0600

    Create config file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const core = require("@actions/core")
 const github = require("@actions/github")
 
@@ -6,6 +7,10 @@ try {
   console.log('Project Id input', core.getInput('project-id'))
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`)
+  fs.writeFileSync('config/.env', core.getInput('project-id'), err => {
+    if (err) throw err
+    console.log('Config file created')
+  })
 } catch (err) {
   core.setFailed(err.message)
 }
